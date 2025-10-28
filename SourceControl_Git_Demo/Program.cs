2863f4917d2cf3a3ada571e06f27a4bce3679d96
V Dinesh Chandra <VDinesh.Chandra@encora.com>
2025-10-28 10:34:40 +0530

    Add project files.

---
+++ SourceControl_Git_Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControl_Git_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
